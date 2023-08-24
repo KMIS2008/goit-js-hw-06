cc4d209510638548cf44067acd05c2ecbea082c2
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2023-08-25 00:20:19 +0300

    task4

---
+++ src/js/task-04.js
@@ -0,0 +1,4 @@
+const counterValue = 0;
+const btnDecrement = document.querySelector([data-action="decrement"]);
+const btnIncrement = document.querySelector([data-action="increment"]);
+console.log(btnDecrement);
