2faee5e651971eb12db6fee2765a26580e12409d
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2023-08-26 01:11:26 +0300

    task8

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,8 +1,8 @@
 const inputRange = document.querySelector('#font-size-control');
 const inputSpan = document.querySelector('#text');
 
-console.dir(inputRange);
-console.log(inputSpan);
+// console.dir(inputRange);
+// console.log(inputSpan);
 
 const handlerRange = (event) => {
 inputSpan.style.fontSize = `${event.currentTarget.value}px`; 
