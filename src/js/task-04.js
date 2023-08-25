bf4efd6b7d3a3af8fbd2c05c5d01d097be4bc539
KMIS2008 <lion.irisha@gmail.com>
2023-08-25 19:35:20 +0300

    task4

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,4 +1,24 @@
-const counterValue = 0;
-const btnDecrement = document.querySelector([data-action="decrement"]);
-const btnIncrement = document.querySelector([data-action="increment"]);
+
+const value = document.querySelector('#value');
+
+// console.log(value);
+let counterValue = 0;
+
+const btnDecrement = document.querySelector('[data-action="decrement"]');
+const btnIncrement = document.querySelector('[data-action="increment"]');
 console.log(btnDecrement);
+
+const handlerClicDecrement = () => {
+ counterValue -=1;
+  value.textContent = counterValue;
+
+};
+
+const handlerClicIncrement= () => {
+     counterValue +=1;
+     value.textContent = counterValue;
+   };
+
+btnDecrement.addEventListener("click", handlerClicDecrement) 
+btnIncrement.addEventListener('click', handlerClicIncrement) 
+
