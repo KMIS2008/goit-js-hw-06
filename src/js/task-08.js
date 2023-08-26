617955e3bad6bca8441492a39f5765fd194cb4f2
KMIS2008 <lion.irisha@gmail.com>
2023-08-26 10:25:04 +0300

    task7,8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
 
+// console.dir(form);
+// console.log(form);
+
+const handlerFormSubmit = (event) => {
+    event.preventDefault();
+    const {
+          elements: { email, password }
+        } = event.currentTarget;
+
+    if (email.value === '' || password.value === '') {
+        return alert('All fields must be filled') 
+    }
+
+    else {
+     console.log(`Email: ${email.value}, Password: ${password.value}`);
+     event.currentTarget.reset();
+    }
+}
+
+form.addEventListener('submit', handlerFormSubmit)
